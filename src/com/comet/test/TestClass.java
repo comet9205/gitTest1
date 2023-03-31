5f98eb7a24f5f89870039c78662d04f4d6f6f9d8
Jang Hye-seong <himedia9205@gmail.com>
2023-03-31 18:02:51 +0900

    곱창

--- src/com/comet/test/TestClass.java
+++ src/com/comet/test/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("나는 마스터다.");
 		System.out.println("집에 가고싶다.");
+		System.out.println("엄마랑 곱창먹을거얌.");
 	}
 	
 	
